1ac18d5326e05c786c785833a524b29e2be58e21
Vibin <vibin.1000@gmail.com>
2019-01-15 00:25:40 -0500

    initial checkins for template

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "terraform_wapp" {
+    ami = ""
+    instance_type = "t2.micro"
+    vpc_security_group_ids =  [ "" ]
+    subnet_id = ""
+    key_name               = "terraform-demo"
+    count         = 1
+    associate_public_ip_address = true
+    tags {
+      Name              = ""
+      Environment       = ""
+      Project           = ""
+    }
+}
+
+output "instance_id_list"     { value = ["${aws_instance.terraform_wapp.*.id}"] }
